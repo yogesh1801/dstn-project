a5ef3c6cf2cd83c866794d08de93d37acdbe86e7
yogesh1801 <114429499+yogesh1801@users.noreply.github.com>
2024-11-24 17:40:05 +0530

    linting fix

--- video_processor/config/conf.py
+++ video_processor/config/conf.py
@@ -1,10 +1,8 @@
-#-------------------------------KAFKA-CONFIG---------------------------#
+# -------------------------------KAFKA-CONFIG---------------------------#
 
-INPUT_BOOTSTRAP_SERVER="192.168.1.114:9092"
-OUTPUT_BOOTSTRAP_SERVER=""
-INPUT_TOPIC="raw-video-data"
-OUTPUT_TOPIC="processed-video-data"
+INPUT_BOOTSTRAP_SERVER = "192.168.1.114:9092"
+OUTPUT_BOOTSTRAP_SERVER = ""
+INPUT_TOPIC = "raw-video-data"
+OUTPUT_TOPIC = "processed-video-data"
 TEMP_DIR = "temp_video"
-MAX_WORKERS=3
-
-
+MAX_WORKERS = 3
--- video_processor/utility/setup_kafka_consumer.py
+++ video_processor/utility/setup_kafka_consumer.py
@@ -1,14 +1,17 @@
 from typing import List
 from kafka import KafkaConsumer
 
-def setup_kafka_consumer(bootstrap_servers: List[str], topic_name: str) -> KafkaConsumer:
+
+def setup_kafka_consumer(
+    bootstrap_servers: List[str], topic_name: str
+) -> KafkaConsumer:
     """Initialize and return Kafka consumer with specified configuration."""
     return KafkaConsumer(
         topic_name,
         bootstrap_servers=bootstrap_servers,
-        key_deserializer=lambda x: x.decode('utf-8') if x else None,
+        key_deserializer=lambda x: x.decode("utf-8") if x else None,
         value_deserializer=None,
-        auto_offset_reset='earliest',
+        auto_offset_reset="earliest",
         enable_auto_commit=True,
-        group_id='video_processor_group'
+        group_id="video_processor_group",
     )
