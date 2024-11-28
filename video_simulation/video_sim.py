eb7cc7a693c6c978161cad5b32d74c83e84c6f20
DarshD4 <f20212836@goa.bits-pilani.ac.in>
2024-11-29 02:09:18 +0530

    update video_sim.py

--- video_simulation/video_sim.py
+++ video_simulation/video_sim.py
@@ -12,11 +12,9 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def get_segment_number(segment_filename):
-    match = re.search(r"segment_(\d+)\.ts", segment_filename)
-    if match:
-        return int(match.group(1))
-    return None
+def get_segment_base_name(segment_filename):
+    """Extracts the segment name without the .ts extension."""
+    return os.path.splitext(segment_filename)[0]
 
 
 def process_video_data():
@@ -31,29 +29,27 @@ def process_video_data():
         segments = sorted(os.listdir(temp_dir))
 
         for segment in segments:
-            segment_number = get_segment_number(segment)
-            if segment_number is None:
+            segment_base_name = get_segment_base_name(segment)
+
+            if segment_base_name in processed_segments:
                 continue
 
             segment_path = os.path.join(temp_dir, segment)
 
-            if segment_number in processed_segments:
-                continue
-
             with open(segment_path, "rb") as f:
                 segment_data = f.read()
 
-            key = str(segment_number).encode("utf-8")
+            key = segment_base_name.encode("utf-8")  # Use full segment name as key
             value = zlib.compress(segment_data, level=6)
 
             future = producer.send(conf.KAFKA_TOPIC, key=key, value=value)
             future.get(timeout=conf.KAFKA_TIMEOUT)
 
             logger.info(
-                f"Sent segment {segment_number} " f"({len(segment_data) / 1024:.2f} KB)"
+                f"Sent segment {segment_base_name} " f"({len(segment_data) / 1024:.2f} KB)"
             )
 
-            processed_segments.add(segment_number)
+            processed_segments.add(segment_base_name)
             # os.remove(segment_path)
 
         time.sleep(0.1)
