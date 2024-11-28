ea3ac2834f8c6870aef6d0c0d27a45f87e683175
yogesh1801 <114429499+yogesh1801@users.noreply.github.com>
2024-11-29 02:32:15 +0530

    scripts updated

--- video_simulation/start_video_sim.sh
+++ video_simulation/start_video_sim.sh
@@ -1,3 +1,8 @@
+docker compose -f docker-compose.kafka.yml down
+sleep 3
+rm -rf kafka-data
+docker compose -f docker-compose.kafka.yml up -d
+sleep 10
 sh create_topic.sh
-sleep 1
+sleep 5
 python video_sim.py
