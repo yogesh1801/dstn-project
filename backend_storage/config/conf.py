ede9a7900465c39672a91888b41613653257440d
yogesh1801 <114429499+yogesh1801@users.noreply.github.com>
2024-11-29 06:42:40 +0530

    backend storage v1

---
+++ backend_storage/config/conf.py
@@ -0,0 +1,6 @@
+BASE_DIR = "data"
+
+DIRECTORIES = {
+    'streams': os.path.join(base_dir, 'streams'),
+    'metadata': os.path.join(base_dir, 'metadata')
+}
---
+++ backend_storage/utility/create_base_dir.py
@@ -0,0 +1,9 @@
+from config import conf
+import os
+
+def create_base_dir():
+    base_dir = conf.BASE_DIR
+    directories = conf.DIRECTORIES
+
+    for dir_path in directories.values():
+        os.makedirs(dir_path, exist_ok=True)
